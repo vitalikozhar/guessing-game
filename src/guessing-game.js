08a8e951bba46815276739aefaaf3d4c90ee2971
Vitali Kozhar <voxfree@pm.me>
2024-09-07 11:38:40 +0400

    feat: task solved

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,29 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = null;
+        this.max = null;
+        this.result = null;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        console.log('min: '+ this.min + ' max: ' + this.max);
     }
 
-    guess() {
-
+     guess() {  //Возвращает число, которое является предположением
+        this.result = Math.round(this.max - (this.max - this.min) / 2);
+        return this.result;
     }
 
-    lower() {
-
+    lower() {   //Вызывается, если предположенное число оказалось больше загаданного
+        this.max = this.result;
+        this.guess();
     }
 
-    greater() {
-
+    greater() {  //Вызывается, если предположенное число оказалось меньше загаданного
+        this.min = this.result;
+        this.guess();
     }
 }
 
